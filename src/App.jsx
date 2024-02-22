d2505dcba142f6d62e875a2de33667e0d8e76557
jatin panesiya <jatinpanesiya2003@gmail.com>
2024-02-22 12:44:29 +0530

    done csv

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,30 @@ const CsvReader = () => {
   const [OTTID, setOTTID] = useState([]);
   const [matchingData, setMatchingData] = useState([]);
 
+  const handleDownloadCsv = () => {
+    if (matchingData.length > 0) {
+      try {
+        const flattenedData = matchingData.map(
+          ({ course, chapter, video }) => ({
+            ...course,
+            ...chapter,
+            ...video,
+          })
+        );
+
+        const csv = Papa.unparse(flattenedData);
+
+        const blob = new Blob([csv], { type: "text/csv" });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "matching_data.csv";
+        link.click();
+      } catch (error) {
+        console.error("Error creating CSV file:", error);
+      }
+    }
+  };
+
   const handleFileChange = (event) => {
     const file = event.target.files[0];
 
@@ -26,7 +50,36 @@ const CsvReader = () => {
         OTTID.flatMap((id) =>
           chapter.Videos.filter(
             (video) => Number(video.streaming_url) === Number(id)
-          )
+          ).map((matchingVideo) => ({
+            course: {
+              SKU: course.SKU,
+              coursePDF: course.coursePDF,
+              description: course.description,
+              Course_id: course.id,
+              course_small_image: course.images.course_small,
+              course_large_image: course.images.course_large,
+              theme_small_image: course.images.theme_small,
+              theme_large_image: course.images.theme_large,
+              intro_description: course.intro_description,
+              intro_streaming_url: course.intro_streaming_url,
+              module_count: course.module_count,
+              name: course.name,
+              runtime: course.runtime,
+              strapline: course.strapline,
+              theme_id: course.theme.theme_id,
+              theme_name: course.theme.theme_name,
+              theme_trailer: course.theme.theme_trailer,
+              trailer: course.trailer,
+              trainer_list: course.trainer_list,
+              video_count: course.video_count,
+            },
+            chapter: {
+              id: chapter.id,
+              name: chapter.name,
+              description: chapter.description,
+            },
+            video: matchingVideo,
+          }))
         )
       )
     );
@@ -44,9 +97,7 @@ const CsvReader = () => {
     <div>
       <input type="file" onChange={handleFileChange} />
 
-      {OTTID.map((data, key) => {
-        return <p key={key}>Id of CSV File : {data}</p>;
-      })}
+      <button onClick={handleDownloadCsv}>Download Matching Data as CSV</button>
     </div>
   );
 };
